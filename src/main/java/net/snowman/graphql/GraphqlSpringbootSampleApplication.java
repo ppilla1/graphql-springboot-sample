e21ff7f0633b5b16963262234eedc1930b01b026
ppilla1 <informprashant@gmail.com>
2019-09-20 12:15:24 +0530

    Project structure creation

---
+++ src/main/java/net/snowman/graphql/GraphqlSpringbootSampleApplication.java
@@ -0,0 +1,13 @@
+package net.snowman.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlSpringbootSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlSpringbootSampleApplication.class, args);
+	}
+
+}
